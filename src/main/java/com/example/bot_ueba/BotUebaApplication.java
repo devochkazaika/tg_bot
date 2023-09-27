7519107b0dc37cc34d237270d51644bcd444937e
Panarin Ilya <cccthafcdh@gmail.com>
2023-09-27 14:34:10 +0700

    first

---
+++ src/main/java/com/example/bot_ueba/BotUebaApplication.java
@@ -0,0 +1,14 @@
+package com.example.bot_ueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BotUebaApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BotUebaApplication.class, args);
+	}
+
+}
